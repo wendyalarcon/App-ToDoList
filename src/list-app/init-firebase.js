7f8f4cc3df4bb4469d8fb70889b5cd854e503751
wendyalarcon <51804236+wendyalarcon@users.noreply.github.com>
2020-08-10 10:04:38 -0500

    primer commit

---
+++ src/list-app/init-firebase.js
@@ -0,0 +1,10 @@
+<firebase-app
+name="list-app"
+auth-domain= "list-app-cb6ee.firebaseapp.com"
+database-url="https://list-app-cb6ee.firebaseio.com"
+api-key="AIzaSyAG6YJpGmf7kaRTvXC9wRHWtue4dPuIWaE"
+storage-bucket="list-app-cb6ee.appspot.com"
+messaging-sender-id="617425893111">
+</firebase-app>
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
